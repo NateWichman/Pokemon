c5e789e6c8593ae7bb25944a8e50d56bd4cfe989
Nate Wichman <wichmann@mail.gvsu.edu>
2018-02-08 15:23:48 -0500

    Practice Project
    Heres a project I made watching youtube videos, it allows a charachter to walk around and switch scenes when you enter tall grass or an enemys vison

---
+++ Pokemon/Assets/Scripts/Map/LongGrass.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LongGrass : MonoBehaviour {
+
+	//Reference enum in Regions.c, determines what type of pokemon appears
+	public BiomeList grassType;
+
+	//Creating a GameManager object from GameManager.cs
+	private GameManager gm;
+
+	// Use this for initialization
+	void Start () {
+		//Setting the gm object equal to the actual game manager (empty object I made) I think
+		gm = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		//When anything goes inside of the collider "col" this code will run
+		if (col.GetComponent<PlayerMovement> ()) {
+	     	// the component colliding has the class player movement attached
+			//Probability = x / 187.5
+			//VC = 10, C = 8.5, SR = 6.75, Rare = 3.33, VR = 1.25
+
+			//Setting up floats for the probability of finding various pokemon based on rarity
+			float vc = 10.0f / 187.5f;
+			float c = 8.5f / 187.5f;
+			float sr = 6.75f / 187.5f;
+			float r = 3.33f / 187.5f;
+			float vr = 1.25f / 187.5f;
+
+			//Variable for probability, add the 'f' to ensure it is a float with .0 too
+			float p = Random.Range (0.0f, 100.0f);
+
+
+		    //These ifs determin which rarity should appear,
+			//Notice that it starts from vr and goes down to vc
+			if (p < vr * 100) {
+				if (gm != null) {
+					gm.EnterBattle (Rarity.VeryRare);
+				}
+			} else if (p < r * 100) {
+				if (gm != null) {
+					gm.EnterBattle (Rarity.Rare);
+				}
+				
+			} else if (p < sr * 100) {
+				if (gm != null) {
+					gm.EnterBattle (Rarity.SemiRare);
+				}
+
+			} else if (p < c * 100) {
+				if (gm != null) {
+					gm.EnterBattle (Rarity.Common);
+				}
+
+			} else if (p < vc * 100) {
+				if (gm != null) {
+					gm.EnterBattle (Rarity.VeryCommon);
+				}
+
+			}
+		}
+	}
+}
---
+++ Pokemon/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Direction currentDir;
+	Vector2 input;
+	bool isMoving = false;
+	Vector3 startPos;
+	Vector3 endPos;
+	float t;
+
+	public Sprite northSprite;
+	public Sprite eastSprite;
+	public Sprite southSprite;
+	public Sprite westSprite;
+
+	public float walkSpeed = 3f;
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isMoving) {
+			input = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+			if (Mathf.Abs (input.x) > Mathf.Abs (input.y)) {
+				input.y = 0; //Makes it so the player does not move diagnoally
+			} 
+			else {
+				input.x = 0;
+			}
+			//The below if statement determins the correct enum to change the sprite
+			if (input != Vector2.zero) {
+				if (input.x < 0){
+					currentDir = Direction.West;
+				} 
+				else if (input.x > 0) {
+					currentDir = Direction.East;
+				} 
+				else if (input.y < 0) {
+					currentDir = Direction.South;
+				} 
+				else if (input.y > 0) {
+					currentDir = Direction.North;
+				}
+
+				//Changes sprite based on currentDir value found above
+				switch (currentDir) {
+				case Direction.North:
+					gameObject.GetComponent<SpriteRenderer> ().sprite = northSprite;
+					break;
+				case Direction.East:
+					gameObject.GetComponent<SpriteRenderer> ().sprite = eastSprite;
+					break;
+				case Direction.South:
+					gameObject.GetComponent<SpriteRenderer> ().sprite = southSprite;
+					break;
+				case Direction.West:
+					gameObject.GetComponent<SpriteRenderer> ().sprite = westSprite;
+					break;
+				}
+				StartCoroutine (Move (transform));
+			}
+		}
+	}
+
+	public IEnumerator Move(Transform entity){
+		isMoving = true; //Signalling that the player is moving
+		startPos = entity.position; //Putting variable startPos to be the players current position
+		t = 0; //Reseting time to 0
+
+		endPos = new Vector3 (startPos.x + System.Math.Sign (input.x), startPos.y + System.Math.Sign(input.y), startPos.z);
+
+		while (t < 1f) {
+			t += Time.deltaTime * walkSpeed;
+			entity.position = Vector3.Lerp (startPos, endPos, t);
+			yield return null;
+		}
+
+		isMoving = false;
+		yield return 0;
+	}
+}
+
+enum Direction{
+	North,
+	East,
+	South,
+	West
+}
